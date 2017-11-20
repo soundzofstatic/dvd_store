61ffb8294e034df193e0f96597a082fffbcc86d4
Unknown <eugenea.herroniii@gmail.com>
2017-11-20 15:51:34 -0600

    Auto stash before merge of "genebranch" and "origin/dev"

---
+++ Inventory.py
@@ -0,0 +1,45 @@
+import csv
+
+class Store:
+
+    __INVENTORYFILE = "products.csv"
+    
+    def __init__(self, taxRate):
+        self.__Inventory = self.setInventory()
+        
+        self.__taxRate = taxRate
+        print(self.__storeInventory
+
+    def getTaxRate(self):
+        return self.__taxRate
+
+
+    def setTaxRate(self, taxRate):
+        self.__taxRate = taxRate     
+
+
+    
+    def getInventory(self):
+        return self.__storeInventory
+
+
+
+    def setInventory(self):
+
+        Inventory = []
+        
+      
+        inventory_list = open(self.__INVENTORYFILE, 'r')
+        
+        for row in inventory_list:
+            Inventory.append(row.strip().split(','))
+
+            
+        return Inventory
+        
+    
+
+def main():
+     storeVisit = Store()   
+
+main()
